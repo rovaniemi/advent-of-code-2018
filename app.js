78878a0f3163d856596f4e8327a4035c82313a90
Mauri Karlin <mauri.karlin@helsinki.fi>
2018-12-01 15:33:40 +0200

    first day

---
+++ app.js
@@ -0,0 +1,4 @@
+import day01 from './day01'
+
+day01.fisrt()
+day01.second()
---
+++ day01/index.js
@@ -0,0 +1,43 @@
+import fs from 'fs'
+const contents = fs.readFileSync(`${__dirname}/input`, 'utf8')
+const arr = contents.toString().split('\n')
+
+const first = () => {
+  const sum = arr.reduce((p, c) => {
+    const sign = c.substring(0, 1)
+    return sign === '+'
+      ? p + parseInt(c.substring(1, c.length))
+      : p - parseInt(c.substring(1, c.length))
+  }, 0)
+  console.log(sum)
+}
+
+const second = () => {
+  const hash = {}
+  let i = 0
+  let freq = 0
+  let answ = undefined
+  while (answ === undefined) {
+    if (hash[freq]) {
+      answ = freq
+    } else {
+      const target = arr[i]
+      const sign = target.substring(0, 1)
+      hash[freq] = true
+      sign === '+'
+        ? (freq += parseInt(target.substring(1, target.length)))
+        : (freq -= parseInt(target.substring(1, target.length)))
+      if (i === arr.length - 1) {
+        i = 0
+      } else {
+        i++
+      }
+    }
+  }
+  console.log(answ)
+}
+
+export default {
+  fisrt: first,
+  second: second
+}
